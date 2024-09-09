92dae11b59072c9b2840e05be81a754f7cec56f9
GermanBluefox <dogafox@gmail.com>
2024-09-09 21:23:45 +0800

    * (bluefox) Fixed polar and bar charts

---
+++ test/testAdapter.gui.js
@@ -0,0 +1,41 @@
+const engineHelper = require('./engineHelper');
+const guiHelper = require('./guiHelper');
+const { existsSync } = require('node:fs');
+const {
+    deleteFoldersRecursive,
+} = require('@iobroker/build-tools');
+let gPage;
+
+async function screenshot(page, fileName) {
+    page = page || gPage;
+    await page.screenshot({ path: `${__dirname}/../tmp/screenshots/${fileName}.png` });
+}
+
+describe.skip('echarts-gui', () => {
+    before(async function () {
+        this.timeout(240_000);
+        // Clean tmp folder
+        if (existsSync(`${__dirname}/../tmp/iobroker-data`)) {
+            deleteFoldersRecursive(`${__dirname}/../tmp/iobroker-data`);
+        }
+
+        // install js-controller, web and vis-2-beta
+        await engineHelper.startIoBroker();
+        const { page } = await guiHelper.startBrowser(process.env.CI === 'true');
+        gPage = page;
+    });
+
+    it('Check echarts', async function () {
+        this.timeout(120_000);
+        await gPage.waitForSelector('.MuiButtonBase-root', { timeout: 120_000 });
+        await screenshot(gPage, '00_started');
+    });
+
+    after(async function () {
+        this.timeout(5_000);
+        await guiHelper.stopBrowser();
+        console.log('BROWSER stopped');
+        await engineHelper.stopIoBroker();
+        console.log('ioBroker stopped');
+    });
+});
