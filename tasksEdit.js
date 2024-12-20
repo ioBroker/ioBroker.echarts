c30e7a080b2ac641c0f9d1455091b20247ad196b
GermanBluefox <dogafox@gmail.com>
2024-12-20 12:05:44 +0000

    Fixing build

--- tasksEdit.js
+++ tasksEdit.js
@@ -40,8 +40,8 @@ if (fs.existsSync(`${__dirname}/src-editor/node_modules`)) {
 }
 installPromise
     .then(() => {
-        if (fs.existsSync(`${__dirname}/src-widgets/node_modules/@iobroker/adapter-react-v5/build`)) {
-            fs.copyFileSync(`${__dirname}/src-widgets/node_modules/@iobroker/adapter-react-v5/build/index.css`, `${__dirname}/src-widgets/node_modules/@iobroker/adapter-react-v5/index.css`);
+        if (fs.existsSync(`${__dirname}/src-editor/node_modules/@iobroker/adapter-react-v5/build`)) {
+            fs.copyFileSync(`${__dirname}/src-editor/node_modules/@iobroker/adapter-react-v5/build/index.css`, `${__dirname}/src-editor/node_modules/@iobroker/adapter-react-v5/index.css`);
         }
     })
     .then(() => buildReact(`${__dirname}/src-editor/`, { rootDir: __dirname }))
