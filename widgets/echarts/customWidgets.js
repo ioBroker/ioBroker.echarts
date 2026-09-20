f4df2cac612e1c9658cc8f963ecbd7589c39158d
GermanBluefox <dogafox@gmail.com>
2026-09-20 21:10:52 +0300

    Added tooltip

--- widgets/echarts/customWidgets.js
+++ widgets/echarts/customWidgets.js
@@ -1,7 +1,7 @@
-import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__echarts__customWidgets_js-DqTJQn56.js";export{t as get,e as init};
+import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__echarts__customWidgets_js-n5ZZg_oT.js";export{t as get,e as init};
 if (typeof document !== 'undefined' && document.head) {
   try {
-    for (const __mfWarmupPath of ["assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__echarts__customWidgets_js-DqTJQn56.js","assets/virtualExposes-Dki_AwQ_.js","assets/virtual_mf-exposes___mfe_internal__echarts__customWidgets_js-CLDCXNQV.js","assets/_virtual_mf-localSharedImportMap___mfe_internal__echarts__mf_owner__1-CUq-kTqB.js","assets/vite-preload-helper-vX0ogsB4.js","assets/dist-D42tNvbP.js","assets/rolldown-runtime-C0FnF6B9.js"]) {
+    for (const __mfWarmupPath of ["assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__echarts__customWidgets_js-n5ZZg_oT.js","assets/virtualExposes-8e1c_wad.js","assets/virtual_mf-exposes___mfe_internal__echarts__customWidgets_js-ClKlJyhK.js","assets/_virtual_mf-localSharedImportMap___mfe_internal__echarts__mf_owner__1-CUq-kTqB.js","assets/vite-preload-helper-vX0ogsB4.js","assets/dist-D42tNvbP.js","assets/rolldown-runtime-C0FnF6B9.js"]) {
       const __mfWarmupLink = document.createElement('link');
       __mfWarmupLink.rel = 'modulepreload';
       __mfWarmupLink.crossOrigin = '';
